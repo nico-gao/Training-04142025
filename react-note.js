bbbbd4cc33b2f33e6b0ebba9a15990dc8fd8aabc
nico-gao <jtgao99@gmail.com>
2025-05-06 12:46:45 -0700

    react boilerplate

---
+++ react-note.js
@@ -0,0 +1,26 @@
+/**
+ * Node
+ *
+ * package.json
+ * package-lock.json
+ * node_modules
+ *
+ * npx - execute the command, create the resources using the package, remove the package
+ * create-react-app
+ *
+ * React
+ *
+ * component based architecture
+ * DOM rendering optimization - virtual DOM
+ * one way data flow - predictable
+ *  app -> navbar/sideNav/content -> buttons/text
+ *
+ *
+ * .jsx -> javascript
+ * .tsx -> typescript
+ *
+ * react -> handles component logic, provides APIs -> virtual DOM
+ * react-DOM -> render components to actual DOM
+ * react-native-DOM
+ *
+ */
